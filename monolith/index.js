274cbfaf91d9a28d65c7e6fce5936c64a5c903dd
Artem Tanyhin <magic.cat.775@gmail.com>
2023-05-25 21:50:47 -0400

    converted monolith into a subgraph

--- monolith/index.js
+++ monolith/index.js
@@ -1,6 +1,8 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 
+const { buildSubgraphSchema } = require("@apollo/subgraph");
+
 const { readFileSync } = require('fs');
 const axios = require('axios');
 const gql = require('graphql-tag');
@@ -18,11 +20,13 @@ const PaymentsAPI = require('./datasources/payments');
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema: buildSubgraphSchema({
+      typeDefs,
+      resolvers,
+    }),
   });
 
-  const port = 4000;
+  const port = 4001;
 
   try {
     const { url } = await startStandaloneServer(server, {
